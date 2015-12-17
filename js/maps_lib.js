773f7746b09dda51d0cc862790c812868fd5a253
adamcpham <adamcpham@users.noreply.github.com>
2015-12-17 04:28:52 -0800

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -163,10 +163,10 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
-        var type_column = "'owned'";
+        var type_column = "'dot'";
 
-if ( $("#rbType1").is(':checked')) self.whereClause += " AND " + type_column + "=1";
-if ( $("#rbType2").is(':checked')) self.whereClause += " AND " + type_column + "=2";
+if ( $("#rbType1").is(':checked')) self.whereClause += " AND " + type_column + "=2";
+if ( $("#rbType2").is(':checked')) self.whereClause += " AND " + type_column + "=1";
 if ( $("#rbType3").is(':checked')) self.whereClause += " AND " + type_column + "=3";
         //-----end of custom filters-----
 
